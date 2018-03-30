e518be6258716465697916393db08fd1ac88e642
JuanCarlosOrtegaSantiago <37173001+JuanCarlosOrtegaSantiago@users.noreply.github.com>
2018-03-30 00:12:54 -0600

    Agregar archivos de proyecto.

---
+++ MiFarmacia/MiFarmacia/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MiFarmacia.Models;
+
+namespace MiFarmacia.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
